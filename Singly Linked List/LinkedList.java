57c82671b173ef5bd53af7136beee6c6f89e2a4d
Rajesh Shinde <98838758+rajesh6001@users.noreply.github.com>
2024-10-08 19:38:31 +0000

    commit

--- Singly Linked List/LinkedList.java
+++ Singly Linked List/LinkedList.java
@@ -24,6 +24,31 @@ public class LinkedList{
 
     }
 
+    public void addbyposition(int val, int pos){
+        Node newnode=new Node(val);
+        if(head==null){
+            head=newnode;
+        }
+        else{
+            if(pos==1){
+                newnode.next=head;
+                head=newnode;
+            }
+            else{
+                Node temp=head;
+                for(int i=0;temp!=null&&i<pos-2;i++){
+                    temp=temp.next;
+                }
+                newnode.next=temp.next;
+                temp.next=newnode;
+
+            }
+
+        }
+
+
+    }
+
     public void display(){
         Node temp=head;
         for(;temp.next!=null;temp=temp.next){
